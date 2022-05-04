69c29335e026243dc683f7905283e5e0a477dc04
Otsuka, Kohei <otsuka_kunikura@outlook.com>
2022-05-04 15:12:34 +0900

    Add document

--- receiptline4android/src/main/java/com/github/otsuka_kohei/receiptline4android/DisplayOption.kt
+++ receiptline4android/src/main/java/com/github/otsuka_kohei/receiptline4android/DisplayOption.kt
@@ -3,6 +3,13 @@ package com.github.otsuka_kohei.receiptline4android
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * Display option of receiptline
+ *
+ * @property charactersPerLine characters per line (default: 48)
+ * @property encoding default: multilingual (including cp437, cp852, cp858, cp866, cp1252)
+ * @property spacing default: no line spacing
+ */
 @Serializable
 data class DisplayOption(
     @SerialName("cpl")
